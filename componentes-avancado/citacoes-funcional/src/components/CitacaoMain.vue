394b2f7628a4fcdaf85ce2c551b48e61dafa621a
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-05-01 18:07:42 -0300

    finalização do modulo de componentes avançados

---
+++ componentes-avancado/citacoes-funcional/src/components/CitacaoMain.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="citacao">
+        <div class="autor">
+            <slot name="autor"></slot>
+        </div>
+        <slot></slot>
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        texto: String
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+    h1{
+        color: aqua;
+    }
+    .autor{
+        float: right;
+    }
+</style>
---
+++ componentes-avancado/desafio-comp-avancado/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+	<div id="app">
+		<span>
+			<button class="vermelho" @click="compDinamico = 'VermelhoApp'">Carregar Componente Vermelho</button>
+			<button class="verde" @click="compDinamico = 'VerdeApp'">Carregar Componente Verde</button>
+			<button class="azul" @click="compDinamico = 'AzulApp'">Carregar Componente Azul</button>
+		</span>
+		
+	
+		<component :is="compDinamico">
+			<span>Conteúdo do componente <strong>{{ compDinamico.slice(0,-3) }}</strong></span>
+		</component>
+	</div>
+</template>
+
+<script>
+import VermelhoApp from './components/VermelhoApp.vue'
+import VerdeApp from './components/VerdeApp.vue'
+import AzulApp from './components/AzulApp.vue'
+
+export default {
+	name: 'app',
+	components: { VermelhoApp, VerdeApp, AzulApp },
+	data(){
+		return{
+			compDinamico: 'VermelhoApp'
+		}
+	}
+}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	button {
+		padding: 10px;
+		color: #FFF;
+		font-size: 1.3rem;
+	}
+
+    .caixa {
+        color: #FFF;
+        font-size: 1.8rem;
+        padding: 25px;
+		margin: 10px 0px;
+    }
+
+	.vermelho {
+        border: 2px solid red;
+        background-color: #f54235;
+    }
+
+	.verde {
+        border: 2px solid green;
+        background-color: #49b057;
+    }
+
+	.azul {
+        border: 2px solid blue;
+        background-color: #2594f0;
+    }
+</style>
